a7b2f2d483df9a39e2a750f27722310712e61b90
DutchBOI1 <136958734+DutchBOI1@users.noreply.github.com>
2023-12-28 02:26:21 +0800

    Update Block.sol

--- Block.sol
+++ Block.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ConditionalTx {
+    address public owner;
+    uint256 public targetBlock;
+    address public destination;
+    uint256 public amountToSend;
+
+    event TransferSent(address indexed _from, address indexed _to, uint256 _amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can execute this");
+        _;
+    }
+
+    constructor(address _destination, uint256 _amountToSend, uint256 _targetBlock) {
+        owner = msg.sender;
+        destination = _destination;
+        amountToSend = _amountToSend;
+        targetBlock = _targetBlock;
+    }
+
+    function sendConditionalTransfer() external onlyOwner {
+        require(block.number >= targetBlock, "Target block not reached yet");
+
+        // Transfer funds to the specified address
+        (bool success, ) = destination.call{value: amountToSend}("");
+        require(success, "Transfer failed");
+
+        // Emit an event
+        emit TransferSent(address(this), destination, amountToSend);
+    }
+}
